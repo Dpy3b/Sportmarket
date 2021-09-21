99e3d48d938ca509b34a2efdbc4d2871443f741b
Dpy3b <57526052+Dpy3b@users.noreply.github.com>
2021-09-21 22:31:43 +0300

    Сделал боковое меню (остались проблемы) + меню поиска по категориям

--- src/js/main.js
+++ src/js/main.js
@@ -4,44 +4,57 @@
   * @param {number} first - первое число
   * @returns {number}
   */
+
 document.querySelector(".icon-menu").addEventListener("click", () => {
 document.querySelector(".icon-menu").classList.toggle("_active"), document.querySelector(".menu__body").classList.toggle("_active")
 });
 
-/*
-const textArea = document.querySelectorAll("[data-autoresize]");
-textArea.forEach(e => {
-    e.addEventListener("input", e => {
-        let t = e.target;
-        t.style.height = "30px", t.style.height = t.scrollHeight + "px"
-    })
-}), document.querySelector(".burger").addEventListener("click", () => {
-    document.querySelector(".burger").classList.toggle("burger--active"), document.querySelector(".sidebar").classList.toggle("sidebar--active")
-}), document.querySelector(".sidebar__btn--violet").addEventListener("click", () => {
-    document.getElementById("contact-modal").classList.add("modal--active")
-}), document.querySelector(".modal__close").addEventListener("click", () => {
-    document.getElementById("contact-modal").classList.remove("modal--active")
+let menuParents = document.querySelectorAll('.menu-page__parent');
+let submenuItems = document.querySelectorAll('.submenu-page__item');
+
+for (let index = 0; index < menuParents.length; index++){
+    const menuParent = menuParents[index];
+    menuParent.addEventListener("mouseenter", function(e){
+        menuParent.classList.add('_active');
+    });
+    menuParent.addEventListener("mouseleave", function(e){
+        menuParent.classList.remove('_active');
+    });
+}
+
+let menuPageBurger = document.querySelector('.menu-page__burger');
+let menuPageBody = document.querySelector('.menu-page__body');
+
+
+menuPageBurger.addEventListener('click', function(e){
+    menuPageBurger.classList.toggle('_active');
+    menuPageBody.classList.toggle('_active');
+    //ваще можно было к одному классу прописать одинаковые стили по идее, но мы экономим время поэтому делаем вот так вот
 });
-const shareLink = document.querySelectorAll(".share-link");
-shareLink.forEach((function (e) {
-    e.addEventListener("click", (function () {
-        document.getElementById("share-modal").classList.add("modal--active")
-    }))
-}));
-const modal__close = document.querySelectorAll(".modal__close");
-modal__close.forEach((function (e) {
-    e.addEventListener("click", (function () {
-        document.getElementById("share-modal").classList.remove("modal--active")
-    }))
-}));
-const storiesItem = document.querySelectorAll(".stories__item");
-storiesItem.forEach((function (e) {
-    e.addEventListener("click", (function () {
-        document.getElementById("stories-modal").classList.add("modal--active")
-    }))
-})), $(document).ready((function () {
-    $("#stories-modal").on("click", (function () {
-        $("#stories-modal").removeClass("modal--active"), $(this).addClass("#stories-modal")
-    }))
-}));
-*/
+
+let searchSelect= document.querySelector('.search-page__select');
+let categoriesSearch = document.querySelector('.categories-search');
+searchSelect.addEventListener('click', function(e){
+    searchSelect.classList.toggle('_active');
+    categoriesSearch.classList.toggle('_active');
+});
+
+let checkboxCategories = document.querySelectorAll('.categories-search__checkbox');
+
+for (let index = 0; index < checkboxCategories.length; index++) {
+    const checkboxCategory = checkboxCategories[index];
+    checkboxCategory.addEventListener('change', function(e){
+        checkboxCategory.classList.toggle('_active');
+
+        let checkboxActiveCategories = document.querySelectorAll('.categories-search__checkbox._active');
+
+        if (checkboxActiveCategories.length > 0){
+            searchSelect.classList.add('_categories');
+            let searchQuanity = document.querySelector('.search-page__quanity');
+            searchQuanity.innerHTML = searchQuanity.getAttribute('data-text') + checkboxActiveCategories.length;
+        } else {
+            searchSelect.classList.remove('_categories');
+        }
+    });
+}
+
